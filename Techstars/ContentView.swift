fee6ae3e41fa2c346c6a50e110bef9a313b8e107
Kiran Mak <kiranamak@gmail.com>
2019-12-12 21:25:29 -0500

    Initial Commit

---
+++ Techstars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Techstars
+//
+//  Created by Kiran Mak on 12/12/19.
+//  Copyright © 2019 Kiran Mak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
